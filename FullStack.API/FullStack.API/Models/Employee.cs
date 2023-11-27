8a1903ac2ad9d2a9abd2d1773579551084f1db26
unknown <med.damak1@gmail.com>
2023-11-27 23:25:29 +0100

    first commit

---
+++ FullStack.API/FullStack.API/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace FullStack.API.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }  
+        public string Department { get; set; }
+            
+       
+    }
+}
